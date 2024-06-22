b299da040c7b987116f0884cad889399e3ebbf5b
samstrong774 <excellentsam2@gmail.com>
2024-06-22 02:49:28 -0700

    print all possible combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - A program that prints all numbers 00-99
+ * main - A program that prints combination of all two digit numbers.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
 
-	for (num = 0; num <= 99; num++)
+	int num = 0;
+	int tens;
+	int ones;
+
+	while (num <= 99)
 	{
-		putchar((num / 10) + '0');
-		putchar((num % 10) + '0');
-		if (num != 99)
+		tens = num % 10;
+		ones = num / 10;
+
+		if (ones < tens)
 		{
-		putchar(',');
-		putchar(' ');
+			putchar(ones + '0');
+			putchar(tens + '0');
+			if (num < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		}
+		num++;
+	}
 	putchar('\n');
+
 	return (0);
 }
