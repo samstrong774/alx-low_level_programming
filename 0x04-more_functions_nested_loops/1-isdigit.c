682cd82fbb66faf56fa25d74c82313931b9f2b47
samstrong774 <excellentsam2@gmail.com>
2024-07-05 16:35:15 -0700

    multiply two numbers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,19 +2,18 @@
 
 /**
  *_isdigit  function that checks for digit from (0-9)
- * @c: parmeters
- * Returns (1) if c is a  digit
- * Returns (0) if c is not a digit
+ * @c: parameters
+ * Return (1) if it is a number else otherwise
  *
  */
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
-	return (1);
+		return (1);
 	}
 	else
 	{
-	return (0);
-}
+		return (0);
+	}	
 }
