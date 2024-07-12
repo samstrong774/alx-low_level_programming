b402e3c210bbe0bd1f5e20b81dd0153a709d3fc5
samstrong774 <excellentsam2@gmail.com>
2024-07-12 00:32:16 -0700

    Return Natural square root of number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * helper - function to loop eith new second variable
+ *
+ * @n: int arg
+ * @result: int arg
+ *
+ * Return: sqaure root
+ */
+
+int helper(int n, int result)
+{
+	if (result * result == n)
+	{
+		return (result);
+	}
+	else if (result * result > n)
+	{
+		return (-1);
+	}
+	else
+		return (helper(n, result + 1));
+}
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ *
+ * @n: int arg
+ *
+ * Return: int
+ */
